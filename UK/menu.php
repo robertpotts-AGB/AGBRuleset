de143506657428edaceb8febf3972560b6ede201
Robert Potts <63243296+robertpotts-AGB@users.noreply.github.com>
2025-12-10 17:03:21 +0000

    Remove duplicate entry for 'Archery GB'

--- UK/menu.php
+++ UK/menu.php
@@ -1,7 +1,7 @@
 <?php
-
+  $ret['AGBR1'][] = 'Archery GB';
 if (!empty($on) && $_SESSION["TourLocRule"] == 'UK' && ($_SESSION["TourLocSubRule"] == 'SetUkJunNationals' || $_SESSION["TourLocSubRule"] == 'SetUkNationals')) {
-    $ret['AGBR1'][] = 'Archery GB';
+  
     $ret['AGBR1']['Teams'] = 'AGB Teams' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/TeamSetup.php';
     
 }
