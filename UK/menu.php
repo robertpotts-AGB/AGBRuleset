02fa5ec335f3898245b187f28f98c4c4260f35ed
Robert Potts <63243296+robertpotts-AGB@users.noreply.github.com>
2025-12-10 17:02:35 +0000

    Fix team setup assignment in UK menu

--- UK/menu.php
+++ UK/menu.php
@@ -3,5 +3,6 @@
 if (!empty($on) && $_SESSION["TourLocRule"] == 'UK' && ($_SESSION["TourLocSubRule"] == 'SetUkJunNationals' || $_SESSION["TourLocSubRule"] == 'SetUkNationals')) {
     $ret['AGBR1'][] = 'Archery GB';
     $ret['AGBR1']['Teams'] = 'AGB Teams' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/TeamSetup.php';
-     $ret['AGBR1']['Teams'] = 'Round Robin Setup' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/RRSetup.php';
+    
 }
+ $ret['AGBR1']['Teams'] = 'Round Robin Setup' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/RRSetup.php';
