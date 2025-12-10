2132c13a408c6282da59eea3d1522a16ce56dd35
Robert Potts <63243296+robertpotts-AGB@users.noreply.github.com>
2025-12-10 17:07:50 +0000

    Refactor menu.php to add Archery GB options

--- UK/menu.php
+++ UK/menu.php
@@ -1,8 +1,11 @@
 <?php
-  $ret['AGBR1'][] = 'Archery GB';
+
 if (!empty($on) && $_SESSION["TourLocRule"] == 'UK' && ($_SESSION["TourLocSubRule"] == 'SetUkJunNationals' || $_SESSION["TourLocSubRule"] == 'SetUkNationals')) {
-  
-    $ret['AGBR1']['Teams'] = 'AGB Teams' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/TeamSetup.php';
-    
+  $ret['AGBR1'][] = 'Archery GB';
+  $ret['AGBR1']['Teams'] = 'AGB Teams' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/TeamSetup.php';
 }
- $ret['AGBR1']['Teams'] = 'Round Robin Setup' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/RRSetup.php';
+if(!empty($_SESSION["TourCode"]) && strpos($_SESSION["TourCode"], "B2B") !== false) {
+  $ret['AGBR1'][] = 'Archery GB';
+  $ret['AGBR1']['Teams'] = 'Rond Robin Setup' . '|' . $CFG->ROOT_DIR . 'Modules/Sets/UK/RRSetup.php';
+}
+
